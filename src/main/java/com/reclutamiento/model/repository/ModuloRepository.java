ce0bdf996f2216274c4eb21c12e85e68ff00eb90
Dwight Huanca <dwightht.anthony955@gmail.com>
2022-10-20 13:08:45 -0500

    se creo repository para modulos

---
+++ src/main/java/com/reclutamiento/model/repository/ModuloRepository.java
@@ -0,0 +1,9 @@
+package com.reclutamiento.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.reclutamiento.model.Modulo;
+
+public interface ModuloRepository extends JpaRepository<Modulo, Integer>{
+	
+}
